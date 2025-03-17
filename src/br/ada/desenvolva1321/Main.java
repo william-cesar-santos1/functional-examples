2163542c6944bb4134d42288bdd078e7e57c1aec
William Cesar Santos <william_cesar_santos@hotmail.com>
2025-03-17 20:00:41 -0300

    Update

--- src/br/ada/desenvolva1321/Main.java
+++ src/br/ada/desenvolva1321/Main.java
@@ -3,10 +3,9 @@ package br.ada.desenvolva1321;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.IntFunction;
-import java.util.function.IntPredicate;
+import java.util.function.*;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -35,15 +34,15 @@ public class Main {
 
         // [tipo]           [nome]       [atribução de valor]
         // função = [(variaveis) -> código executável]
-//        IntFunction<String> converter3 = (numero) -> "O numero é: " + numero;
-//        System.out.println(converter3.apply(5));
-//
-//        IntPredicate par = numero -> numero % 2 == 0;
-//
-//        IntStream.range(1, 101)
-//                .filter(par)
-//                .mapToObj(converter3)
-//                .forEach(System.out::println);
+        IntFunction<String> converter3 = (numero) -> "O numero é: " + numero;
+        System.out.println(converter3.apply(5));
+
+        IntPredicate par = numero -> numero % 2 == 0;
+
+        IntStream.range(1, 101)
+                .filter(par)
+                .mapToObj(converter3)
+                .forEach(System.out::println);
 
         // Dado que possuo um BigDecimal implemente a conversão para string.
         // Deve ser utilizado a interface Function
@@ -55,10 +54,25 @@ public class Main {
         // Dado que possuo uma string que representa a data no formato dd/MM/yy
         // escreva um conversor para o tipo LocalDate
         // Deve ser utilizado a interface Function
+        // Function -> conversões
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yy");
         Function<String, LocalDate> dateConverter = (dateAsString) -> LocalDate.parse(dateAsString, dateFormatter);
         var date = dateConverter.apply("14/03/25");
         System.out.println(date);
+
+        // Predicate -> filtros (o retorno é um boolean que informa se a expressão foi atendida)
+        Predicate<String> nomesCom4Caracteres = (nome) -> nome.length() == 4;
+        var tem4caracteres = nomesCom4Caracteres.test("William");
+        System.out.println(tem4caracteres);
+
+        // Consumer -> recebe uma info e trabalha com ela
+        Consumer<String> imprimeNome = (nome) -> System.out.println(nome);
+        imprimeNome.accept("William César dos Santos");
+
+        // Supplier -> fornece um dado para a aplicação
+        Supplier<String> meuNome = () -> "William César dos Santos";
+        System.out.println(meuNome.get());
+
 
     }
 
---
+++ src/br/ada/desenvolva1321/exercises/RemocaoItemLista.java
@@ -0,0 +1,31 @@
+package br.ada.desenvolva1321.exercises;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemocaoItemLista {
+
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = pessoas();
+        // Filtre as pessoas com menos de 4 caracteres no primeiro nome
+        // e as remova da lista.
+//        pessoas.removeIf()
+    }
+
+    public static List<Pessoa> pessoas() {
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa(1L, "Alice Johnson", "1234567890", LocalDate.of(1985, 11, 15)));
+        pessoas.add(new Pessoa(2L, "Bob Smith", "9876543210", LocalDate.of(1990, 5, 20)));
+        pessoas.add(new Pessoa(3L, "Bob Brown", "5555555555", LocalDate.of(1975, 9, 30)));
+        pessoas.add(new Pessoa(4L, "David Lee", "4444444444", LocalDate.of(1995, 2, 10)));
+        pessoas.add(new Pessoa(5L, "Emily Davis", "3333333333", LocalDate.of(1988, 7, 25)));
+        pessoas.add(new Pessoa(6L, "Frank Wilson", "2222222222", LocalDate.of(1970, 12, 5)));
+        pessoas.add(new Pessoa(7L, "Grace Miller", "1111111111", LocalDate.of(1992, 3, 18)));
+        pessoas.add(new Pessoa(8L, "Henry Baker", "0000000000", LocalDate.of(1982, 8, 7)));
+        pessoas.add(new Pessoa(9L, "Isabella Green", "9999999999", LocalDate.of(1997, 4, 12)));
+        pessoas.add(new Pessoa(10L, "Jack Taylor", "8888888888", LocalDate.of(1978, 6, 23)));
+        return pessoas;
+    }
+
+}
